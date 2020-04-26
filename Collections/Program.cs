77f57b725c855662bdf5b0eaf157a935b19b36de
Birol Pierce <birolly@outlook.com.tr>
2020-04-26 20:52:33 +0300

    changes

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,24 +13,88 @@ namespace Collections
         static void Main(string[] args)
         {
             //ArrayList();
-            List<string> cities=new List<string>();
+            //List();
+            Dictionary<string,string> dictionary =new Dictionary<string, string>();
+            dictionary.Add("book","kitap");
+            dictionary.Add("table","tablo");
+            dictionary.Add("computer","bilgisayar");
+
+            //Console.WriteLine(dictionary["table"]);  //sonucu tablo olarak verdi
+            //Console.WriteLine(dictionary["glass"]); //olmadığı için hata verdi.
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            Console.WriteLine(dictionary.ContainsKey("glass")); //false verdi
+            Console.WriteLine(dictionary.ContainsKey("table")); //true verdi
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void List()
+        {
+            List<string> cities = new List<string>();
             cities.Add("Ankara");
             cities.Add("İstanbul");
+
+            Console.WriteLine(cities.Contains("Ankara"));
 
             foreach (var city in cities)
             {
                 Console.WriteLine(city);
             }
-            List<Customer> customers=new List<Customer>();
-            customers.Add(new Customer{Id=1,FirstName = "Engin"});
-            customers.Add(new Customer{Id=2,FirstName = "Derin"});
+            //List<Customer> customers = new List<Customer>();
+            //customers.Add(new Customer { Id = 1, FirstName = "Engin" });
+            //customers.Add(new Customer { Id = 2, FirstName = "Derin" });
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer {Id = 1, FirstName = "Engin"},
+                new Customer {Id = 2, FirstName = "Derin"}
+            };
+
+
+            var customer2 = new Customer
+            {
+                Id = 3,
+                FirstName = "Salih"
+            };
+            customers.Add(customer2);
+            customers.AddRange(new Customer[2]
+            {
+                new Customer {Id = 4, FirstName = "Ali"},
+                new Customer {Id = 5, FirstName = "Ayşe"}
+            });
+
+            Console.WriteLine(customers.Contains(customer2));
+
+            //customers.Clear();  // TEMİZLEME
+
+            var index = customers.IndexOf(customer2);
+            Console.WriteLine("Index : {0}", index);
+
+            customers.Add(customer2);
+            var index2 = customers.LastIndexOf(customer2);
+            Console.WriteLine("Index : {0}", index2);
+
+            customers.Insert(0, customer2);
+
+            //customers.Remove(customer2);
+            //customers.Remove(customer2);
+            //customers.Remove(customer2);
+            customers.RemoveAll(c => c.FirstName == "Salih"); //"Salih olanların hepsini siler.
 
             foreach (var customer in customers)
             {
                 Console.WriteLine(customer.FirstName);
             }
 
-            Console.ReadLine();
+            var count = customers.Count;
+            Console.WriteLine("Count: {0}", count);
         }
 
         private static void ArrayList()
